059ed4e1e34828f98384a02fe9d77d5ebe6ecd7a
LeonardoSousa89 <leoprofessionallogytech@outlook.com>
2025-09-15 00:01:37 -0300

    add new features in terms and introduced cypress

---
+++ cypress/cypress.config.js
@@ -0,0 +1,16 @@
+// cypress.config.js
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://127.0.0.1:5500/web", // URL padrão do seu projeto
+    supportFile: "cypress/support/e2e.js", // suporte global
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // onde ficam os testes
+    setupNodeEvents(on, config) {
+      // Adicione plugins ou tasks customizadas aqui se precisar
+      return config;
+    },
+  },
+  video: false, // grava vídeos da execução
+  screenshotOnRunFailure: false, // tira screenshot se falhar
+});
---
+++ cypress/cypress/e2e/contactForm.cy.js
@@ -0,0 +1,43 @@
+// cypress/e2e/contactForm.cy.js
+
+describe("Formulário de Contato - Hodevweb", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:5500/web/index.html"); // ajuste se rodar em outra porta
+  });
+
+  it("Deve preencher e enviar o formulário com sucesso", () => {
+    // Preencher campos
+    cy.get("#nome").type("Leonardo Sousa");
+    cy.get("#email").type("leonardo@email.com");
+    cy.get("#tel").type("+5511999999999");
+    cy.get("#msg").type("Quero mais informações sobre seus serviços!");
+
+    // Enviar formulário
+    cy.get("#send").click();
+
+    // Modal de LGPD deve aparecer
+    cy.get("#terms-modal").should("exist");
+
+    // Marca checkbox
+    cy.get("#lgpdAcceptCheck").check({ force: true });
+
+    // Ativa botão de aceitar
+    cy.get("#lgpdAccept").should("not.be.disabled").click();
+
+    // Confirma que aparece mensagem de sucesso
+    cy.get("#formAlert")
+      .should("exist")
+      .and("have.class", "success");
+    cy.get("#formAlertMessage")
+      .should("contain.text", "Formulário enviado com sucesso!");
+  });
+
+  it("Deve exibir erro se tentar enviar sem preencher os campos", () => {
+    cy.get("#send").click();
+
+    cy.get("#nome").should("have.class", "error");
+    cy.get("#email").should("have.class", "error");
+    cy.get("#tel").should("have.class", "error");
+    cy.get("#msg").should("have.class", "error");
+  });
+});
